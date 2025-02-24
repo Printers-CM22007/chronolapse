84de0b6c241061cb46e8836384fc86643cdc1b2f
Alecfore <alecfore@gmail.com>
2025-02-24 17:54:15 +0000

    Inital work on timelapse buffer

--- lib/backend/timelapse_buffer.dart
+++ lib/backend/timelapse_buffer.dart
@@ -1,60 +1,83 @@
+import 'dart:typed_data';
+
 import 'package:opencv_dart/opencv.dart' as cv2;
-import 'native_image.dart';
 import 'dart:async';
 import 'dart:isolate';
 
 class TimelapseBuffer {
   late SendPort _sendPort;
   final Completer<void> _isolateReady = Completer.sync();
+  late ReceivePort _receivePort;
 
-  List<cv2.Mat> frames = List.empty();
-  int nFrames = 0;
+  List<cv2.Mat> frames = [];
 
-  Future<void> spawn() async {
-    final recievePort = ReceivePort();
-    recievePort.listen(_handleResponsesFromIsolate);
-    await Isolate.spawn(_startRemoteIsolate, recievePort.sendPort);
+  Future<void> beginLoadingTimelapse(String timelapsePath, int nFrames) async {
+    _receivePort = ReceivePort();
+    _receivePort.listen(_handleResponsesFromIsolate);
+    await Isolate.spawn(_startRemoteIsolate, _receivePort.sendPort);
+
+    await _isolateReady.future;
+    Map<String, dynamic> message = {
+      "nFrames": nFrames,
+      "timelapsePath": timelapsePath
+    };
+    _sendPort.send(message);
   }
 
   void _handleResponsesFromIsolate(dynamic message) {
     if (message is SendPort) {
       _sendPort = message;
       _isolateReady.complete();
-    } else {
-      print("message is $message");
-      //var img = NativeImage();
-      //var img = cv2.imread(message);
-      //frames.add(img);
-      //nFrames++;
-      //_sendPort.send("yo");
+    } else if (message is (bool, Uint8List)) {
+      var decode = cv2.imdecode(message.$2, cv2.IMREAD_COLOR);
+
+      frames.add(decode);
+    } else if (message == "stop") {
+      _receivePort.close();
     }
   }
 
   static void _startRemoteIsolate(SendPort port) {
-    final recievePort = ReceivePort();
-    port.send(recievePort.sendPort);
+    final receivePort = ReceivePort();
+    port.send(receivePort.sendPort);
+
+    final Completer<void> beginProcessing = Completer.sync();
+
+    late int nFrames;
+    late String timelapsePath;
 
     Future(() async {
-      while (true) {
-        // This simulates some background task running
-        //await Future.delayed(Duration(milliseconds: 150));
-        //print("Background task is running...");
-      
+      await beginProcessing.future;
+
+      for(int i = 0; i < nFrames; i++) {
+        var img = await cv2.imreadAsync(timelapsePath + i.toString() + ".png");
+        var encoded = cv2.imencode('.png', img);
+
+        port.send(encoded);
       }
+
+      port.send("stop");
+
+      receivePort.close();
+      Isolate.exit();
     });
 
-    recievePort.listen((dynamic message) async {
-      if (message is String) {
-        final transformed = message + "_bruh";
-        port.send(transformed);
+    receivePort.listen((dynamic message) async {
+      if (message is Map<String, dynamic>) {
+        nFrames = message["nFrames"];
+        timelapsePath = message["timelapsePath"];
+        beginProcessing.complete();
       }
     });
-
-
   }
 
-  Future<void> doStuff(String string) async {
+  Future<cv2.Mat> retrieveFrame(int frame) async {
     await _isolateReady.future;
-    _sendPort.send(string);
+
+    while (frames.length <= frame) {
+      await Future.delayed(Duration(milliseconds: 10));
+    }
+
+    return frames[frame];
   }
 }
--- lib/backend/timelapse_storage.dart
+++
@@ -1,5 +0,0 @@
-class TimelapseStorage {
-  int GetFrame(int frame) {
-    return 0;
-  }
-}
