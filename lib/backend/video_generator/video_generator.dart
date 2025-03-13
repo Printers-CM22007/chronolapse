1441bbf5a5fb901a2039d051aff8ea2827ea75e1
Alec Ratchfrd <alecfore@gmail.com>
2025-03-13 18:08:13 +0000

    organized folders and added video generator test file

--- lib/backend/video_generator/video_generator.dart
+++ lib/backend/video_generator/video_generator.dart
@@ -1,11 +1,11 @@
-import 'package:chronolapse/backend/generator_options.dart';
+import 'package:chronolapse/backend/video_generator/generator_options.dart';
 import 'package:chronolapse/backend/image_transformer/frame_transforms.dart';
 import 'package:chronolapse/backend/image_transformer/image_transformer.dart';
-import 'package:chronolapse/backend/timelapse_buffer.dart';
+import 'package:chronolapse/backend/video_generator/timelapse_buffer.dart';
 import 'package:chronolapse/backend/timelapse_storage/frame/timelapse_frame.dart';
 import 'package:chronolapse/backend/timelapse_storage/timelapse_data.dart';
 import 'package:chronolapse/backend/timelapse_storage/timelapse_store.dart';
-import 'package:chronolapse/backend/video_compiler.dart';
+import 'package:chronolapse/backend/video_generator/video_compiler.dart';
 import 'package:opencv_dart/opencv.dart' as cv2;
 
 class VideoGenerator {
