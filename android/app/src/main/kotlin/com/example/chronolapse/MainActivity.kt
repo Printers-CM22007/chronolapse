679fccafd23dffd502f8ed16b3ae16eefb378fe7
Alec Ratchfrd <alecfore@gmail.com>
2025-03-15 14:10:05 +0000

    MediaCodec inital code

--- android/app/src/main/kotlin/com/example/chronolapse/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chronolapse/MainActivity.kt
@@ -5,6 +5,9 @@ import android.media.MediaScannerConnection
 import android.net.Uri
 import android.provider.MediaStore
 import android.util.Log
+import android.media.MediaCodec
+import android.media.MediaFormat
+import android.media.MediaMuxer
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -21,10 +24,34 @@ class MainActivity: FlutterActivity() {
             if (call.method == "testFunction") {
                 val count = call.argument<Int>("count")!!
                 result.success(count + 1)
-            }
-            else {
+            } else if (call.method == "testMediaCodec") {
+                testMediaCodec();
+                result.success(true);
+            } else {
                 result.notImplemented()
             }
         }
+    }
+
+    private fun testMediaCodec() {
+        Log.println(Log.DEBUG, "", "test")
+        val mediaFormat = android.media.MediaFormat.createVideoFormat(android.media.MediaFormat.MIMETYPE_VIDEO_AVC, 1920, 1080).apply {
+            setInteger(android.media.MediaFormat.KEY_BIT_RATE, 1000000)
+            setInteger(android.media.MediaFormat.KEY_FRAME_RATE, 30)
+            setInteger(android.media.MediaFormat.KEY_I_FRAME_INTERVAL, 10)
+        }
+
+        val codec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC).apply {
+            configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
+            start()
+        }
+
+        /*val mediaMuxer = MediaMuxer(outputFile.absolutePath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4).apply {
+            val videoTrackIndex = addTrack(mediaFormat)
+            start()
+        }
+
+        val bufferInfo = MediaCodec.BufferInfo()
+        val outputBuffer = ByteBuffer.allocate(1024 * 1024)  // Buffer to hold encoded data*/
     }
 }
