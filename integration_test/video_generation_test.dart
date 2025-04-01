5c510916d1414cf552f17e8c7576dd2b91b9a3a3
Alec Ratchfrd <alecfore@gmail.com>
2025-04-01 16:56:46 +0100

    finalized video gen test

--- integration_test/video_generation_test.dart
+++ integration_test/video_generation_test.dart
@@ -1,5 +1,7 @@
 import 'package:chronolapse/main.dart';
 import 'package:chronolapse/ui/pages/dashboard_page/dashboard_page.dart';
+import 'package:chronolapse/ui/pages/export_page.dart';
+import 'package:chronolapse/ui/shared/video_player_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:chronolapse/util/shared_keys.dart';
@@ -42,18 +44,30 @@ void main() async {
     await tester.tap(find.byKey(photoTakingShutterButtonKey));
     // waits for response from the camera API
     await tester.pumpAndSettle(const Duration(seconds: 3));
+
     expect(find.text("Continue"), findsOne);
     await tester.tap(find.text("Continue"));
     await tester.pumpAndSettle();
 
     expect(find.text("Place at least 4 markers on the image"), findsOne);
-
-    final random = Random();
     final editorRect = tester.getRect(find.byKey(featurePointsEditorKey));
-    await tester.tapAt(Offset(editorRect.left, editorRect.top));
-    await tester.tapAt(Offset(editorRect.left, editorRect.bottom));
-    await tester.tapAt(Offset(editorRect.right, editorRect.top));
-    await tester.tapAt(Offset(editorRect.right, editorRect.bottom));
+    await tester.tapAt(editorRect.center);
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Create"));
+    await tester.pumpAndSettle();
+
+    // Create 4 feature points
+    await tester.tapAt(editorRect.center + const Offset(50, 0));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Create"));
+    await tester.pumpAndSettle();
+    await tester.tapAt(editorRect.center + const Offset(0, 50));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Create"));
+    await tester.pumpAndSettle();
+    await tester.tapAt(editorRect.center + const Offset(50, 50));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Create"));
     await tester.pumpAndSettle();
 
     await tester.tap(find.text("Save and continue"));
@@ -73,5 +87,33 @@ void main() async {
     expect(find.text("Continue"), findsOne);
     await tester.tap(find.text("Continue"));
     await tester.pumpAndSettle();
+    await tester.tap(find.text("Save and exit"));
+    await tester.pumpAndSettle();
+
+    // Take third photo
+
+    await tester.tap(find.text("Take photo"));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byKey(photoTakingShutterButtonKey));
+    // waits for response from the camera API
+    await tester.pumpAndSettle(const Duration(seconds: 3));
+
+    expect(find.text("Continue"), findsOne);
+    await tester.tap(find.text("Continue"));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Save and exit"));
+    await tester.pumpAndSettle();
+
+    // Export video
+
+    await tester.tap(find.text("Export"));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Generate Timelapse"));
+    await tester.pumpAndSettle(const Duration(seconds: 3));
+
+    // Verify the video player is showing
+    final videoPlayer = find.byType(VideoPlayerWidget);
+    expect(videoPlayer, findsOneWidget);
   });
 }
